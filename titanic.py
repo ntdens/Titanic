cb7f44e098a70b49b2bd84748bd61b1808198366
ntdens <ntdens@gmail.com>
2018-07-17 10:40:57 -0400

    Things are in nice functions now

--- titanic.py
+++ titanic.py
@@ -2,42 +2,47 @@ import pandas as pd
 import plotly.offline as py
 import plotly.graph_objs as go
 
+
 def main():
     titanic = pd.read_csv('titanic_data.csv')
     titanic['Survived'] = titanic['Survived'].astype(bool)
     titanic.set_index('PassengerId')
     total_length = len(titanic.index)
-
-    sex = titanic['Sex'].value_counts()
-    print(sex)
-    male_v_female(titanic)
+    sex_data(titanic)
 
 
+# Prints bar charts based on sex data
+def sex_data(df):
+    survive_sex = survive(df, 'Sex')
+    total_sex = df['Sex'].value_counts()
+    deceased_sex = total_sex - survive_sex
+    bar(survive_sex, deceased_sex, 'stack', 'stack.html', 'Survivors', 'Deceased')
+    bar(survive_sex, total_sex, 'group', 'group.html', 'Survivors', 'Total')
 
-def male_v_female(df):
-    survive = df.groupby('Sex').sum()['Survived']
-    total = df['Sex'].value_counts()
+
+# Returns a series with the total survivors of a column
+def survive(df, col):
+    return df.groupby(col).sum()['Survived']
+
+
+# Takes two pandas series and a bar chart type and prints that bar chart.
+def bar(first, second, type, filename, first_name='First', second_name='Second'):
     trace1 = go.Bar(
-        x = survive.index.tolist(),
-        y = survive.values,
-        name = 'Survived'
+        x=first.index.tolist(),
+        y=first.values,
+        name=first_name
     )
     trace2 = go.Bar(
-        x = total.index.tolist(),
-        y = total.values,
-        name = 'Total'
+        x=second.index.tolist(),
+        y=second.values,
+        name=second_name
     )
-    data = [trace2, trace1]
+    data = [trace1, trace2]
     layout = go.Layout(
-        barmode='group'
+        barmode=type
     )
     fig = go.Figure(data=data, layout=layout)
-    py.plot(fig, filename='bar.html', auto_open=True)
-
-
-
-
-
+    py.plot(fig, filename=filename, auto_open=True)
 
 
 if __name__ == '__main__':
