2b56c09e32446f2696a02fa586b95754bbc6d4e7
ntdens <ntdens@gmail.com>
2018-07-18 12:36:18 -0400

    fun with functions

--- titanic.py
+++ titanic.py
@@ -10,6 +10,7 @@ def main():
     total_length = len(titanic.index)
     total_class = titanic.groupby('Pclass').count()['PassengerId']
 
+    fare_data(titanic, 'Pclass')
     # Grabbing the average fare data
     print(multi(titanic, 'Embarked', 'Pclass'))
     # Grabbing the number of each ticket sold
@@ -27,7 +28,7 @@ def survive_total(df, col):
 
 
 # Creating a multiindex dataframe and performs groupby functions on it
-def multi(df, index1, index2, func= 'mean'):
+def multi(df, index1, index2, func='mean'):
     new = df.set_index([index1, index2, df.index])
     new = new.sort_index()
     if func == 'mean':
@@ -35,6 +36,7 @@ def multi(df, index1, index2, func= 'mean'):
     elif func == 'count':
         group = new.groupby(level=[index1, index2]).count()['Fare']
     return group
+
 
 # Prints bar charts based on data
 def bar_data(df, col, type):
@@ -50,24 +52,26 @@ def bar_data(df, col, type):
 
 
 # Sorts bar data based on if the user wants a stacked or grouped chart
-def barsort(survive, total, deceased, type, typestr):
-    survive_bar = bar_input(survive, 'Survivors')
-    total_bar = bar_input(total, 'Total')
-    deceased_bar = bar_input(deceased, 'Deceased')
-    if type == 'stack':
+def barsort(survive, total, deceased, gtype, typestr):
+    survive_bar = trace_input(survive, 'Survivors')
+    total_bar = trace_input(total, 'Total')
+    deceased_bar = trace_input(deceased, 'Deceased')
+    layout = lay('Survival Based on ' + str(typestr), gtype, typestr, 'Number of Passengers')
+    if gtype == 'stack':
         data = [survive_bar, deceased_bar]
-        bar(data, type, 'Survival Based on ' + str(typestr), typestr, 'Number of Passengers')
-    elif type == 'group':
+        graph(data, layout)
+    elif gtype == 'group':
         data = [survive_bar, total_bar]
-        bar(data, type, 'Survival Based on ' + str(typestr), typestr, 'Number of Passengers')
+        graph(data, layout)
 
 
 # Prints histogram based on age data
 def age_data(df):
     survive_age = survive_count(df, 'Age')
     total_age = df['Age']
-    hist(total_age, survive_age, 'overlay', 'Survival Based on Age', 'Total', 'Survivors', 'Age',
-         'Number of Passengers')
+    data = [trace_input(total_age, 'Total', 'hist'), trace_input(survive_age, 'Survivors', 'hist')]
+    layout = lay('Survival Based on Age', 'overlay', 'Age', 'Number of Passengers')
+    graph(data, layout)
 
 
 # Creates bar graphs based on fare data
@@ -78,31 +82,40 @@ def fare_data(df, col):
     survive_fare = survive_fare.join(sur_fare, how='right')
     survive_fare = survive_fare.join(df[col], how='left')
     survive_fare = survive_fare.groupby(col).mean()['Fare']
-    bar_avg_fare = bar_input(avg_fare, 'Average Fare')
-    bar_survive_fare = bar_input(survive_fare, 'Survivor Fare')
+    bar_avg_fare = trace_input(avg_fare, 'Average Fare')
+    bar_survive_fare = trace_input(survive_fare, 'Survivor Fare')
     data = [bar_avg_fare, bar_survive_fare]
     if col == 'Pclass':
-        bar(data, 'group', 'Cost of Tickets by Class of Survivors vs Average', 'Fare by Class', 'Price')
+        layout = lay('Cost of Tickets by Class of Survivors vs Average', 'group', 'Fare by Class', 'Price')
+        graph(data, layout)
     elif col == 'Embarked':
-        bar(data, 'group', 'Cost of Tickets by Port of Survivors vs Average', 'Fare by Port', 'Price')
+        layout = lay('Cost of Tickets by Port of Survivors vs Average', 'group', 'Fare by Port', 'Price')
+        graph(data, layout)
 
 
 # Creates traces for the bar graph data
-def bar_input(series, name=''):
-    trace = go.Bar(
-        x=series.index.tolist(),
-        y=series.values,
-        name=name
-    )
+def trace_input(series, name='', graphtype='bar'):
+    if graphtype == 'bar':
+        trace = go.Bar(
+            x=series.index.tolist(),
+            y=series.values,
+            name=name
+        )
+    if graphtype == 'hist':
+        trace = go.Histogram(
+            histfunc='sum',
+            xbins=dict(start=0, end=100, size=1),
+            x=series,
+            name=name
+        )
     return trace
 
 
-# Creates a comparative bar chart.
-def bar(data, type, title, xtitle='', ytitle=''):
-    data = data
+# Defines the layout of the graph
+def lay(title, gtype, xtitle='', ytitle=''):
     layout = go.Layout(
         title=title,
-        barmode=type,
+        barmode=gtype,
         xaxis=dict(
             title=xtitle
         ),
@@ -110,37 +123,13 @@ def bar(data, type, title, xtitle='', ytitle=''):
             title=ytitle
         )
     )
-    fig = go.Figure(data=data, layout=layout)
-    py.plot(fig, filename=(str(title) + '.html'), auto_open=True)
+    return layout
 
 
-# Creates a comparative histogram
-def hist(first, second, type, title, first_name='First', second_name='Second', xtitle='', ytitle=''):
-    trace1 = go.Histogram(
-        histfunc='sum',
-        xbins=dict(start=0, end=100, size=1),
-        x=first,
-        name=first_name
-    )
-    trace2 = go.Histogram(
-        histfunc='sum',
-        xbins=dict(start=0, end=100, size=1),
-        x=second,
-        name=second_name
-    )
-    data = [trace1, trace2]
-    layout = go.Layout(
-        title=title,
-        barmode=type,
-        xaxis=dict(
-            title=xtitle
-        ),
-        yaxis=dict(
-            title=ytitle
-        )
-    )
+# Creates a graph.
+def graph(data, layout):
     fig = go.Figure(data=data, layout=layout)
-    py.plot(fig, filename=(str(title) + '.html'), auto_open=True)
+    py.plot(fig, filename='bar.html', auto_open=True)
 
 
 if __name__ == '__main__':
